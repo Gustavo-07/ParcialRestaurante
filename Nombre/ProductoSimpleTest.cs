1950059ae3b36a0df9c96add2a7b4c5be3b98cd7
Gustavo-07 <gustavo-garcia1994@hotmail.com>
2020-10-05 16:24:17 -0500

    test No Puede retirar producto simple sin existencia

--- Nombre/ProductoSimpleTest.cs
+++ Nombre/ProductoSimpleTest.cs
@@ -106,5 +106,31 @@ namespace InventarioRestaurante.Core.Domain.Test
             //Validación o verificación 
             Assert.AreEqual("la cantidad para el registro de salida de productos es incorrecta",resultado);
         }
+
+
+        //Escenario 05: No Puede retirar producto simple sin existencia
+        //HU1: COMO USUARIO QUIERO REGISTRAR LA SALIDA DE PRODUCTOS.
+        //Criterio de Aceptación:
+        //1  La cantidad de la salida de debe ser mayor a 0.
+        //Ejemplo
+        //Dado El usuario diligencia una salida a un producto: código 001, 
+        //     nombre “gaseosa litro”, utilidad “ventaDirecta” costo 2000, precio 5000. 
+        //     Con una cantidad de 0 unidades en el sistema.
+        //Cuando Va a registrar una cantidad de 2.
+        //Entonces El sistema presentará el mensaje "el producto que desea retirar 
+        //         no tiene existencia".
+
+        [Test]
+        public void NoPuedeRetirarProductoSimpleSinExistenciaTest()
+        {
+            //Preparar
+            var productoSimple = new ProductoSimple(codigo: 001, nombre: "gaseosa litro", utilidad: "ventaDirecta", costo: 2000, precio: 5000);
+
+            //Accion
+            var resultado = productoSimple.Retirar(2);
+
+            //Validación o verificación 
+            Assert.AreEqual("el producto que desea retirar no tiene existencia", resultado);
+        }
     }
 }
